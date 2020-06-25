044eae97a69bd28ac9e5a42712520403f6920019
ahidiroglu <kutayhidiroglu@gmail.com>
2020-06-25 00:38:22 +0000

    recent changes

--- Class4/backend.tf
+++ Class4/backend.tf
@@ -1,7 +1,7 @@
 terraform {
-backend "s3" {
-bucket = "terraform-state-april-class-ali"
-key = "jenkins/us-east-1/tools/oregon/asg.tfstate"
-region = "us-east-1"
+  backend "s3" {
+    bucket = "terraform-state-april-class-ali"
+    key    = "jenkins/us-east-1/tools/oregon/asg.tfstate"
+    region = "us-east-1"
   }
 }
